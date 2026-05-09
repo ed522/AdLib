accfa79188edcbbcf91dcb2c28e7c82b15e41cf9
ed522 <147557022+ed522@users.noreply.github.com>
2026-05-09 00:53:04 -0400

    Add status readout to server screen

--- AdLib/ViewModel/ServerViewModel.cs
+++ AdLib/ViewModel/ServerViewModel.cs
@@ -1,4 +1,7 @@
 using System;
+
+using AdLib.View;
+
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
@@ -6,38 +9,23 @@ namespace AdLib.ViewModel;
 
 public partial class ServerViewModel : PageViewModelBase
 {
-    public override Type ViewType => typeof(View.ServerScreen);
+    [ObservableProperty] private string _connectedClient = "None";
+    [ObservableProperty] private string _currentTransferName = "No active transfer";
+    [ObservableProperty] private string _selectedCertificate = string.Empty;
+
+    [ObservableProperty] private string _selectedPath = string.Empty;
+    [ObservableProperty] private string _status = "Disconnected";
+    [ObservableProperty] private double _transferProgress;
+    public override Type ViewType => typeof(ServerScreen);
     public override string Title => "Hosting Server";
 
-    [ObservableProperty]
-    private string _selectedPath = string.Empty;
-
-    [ObservableProperty]
-    private string _selectedCertificate = string.Empty;
-
-    [ObservableProperty]
-    private string _connectedClient = "None";
-
-    [ObservableProperty]
-    private string _currentTransferName = "No active transfer";
-
-    [ObservableProperty]
-    private double _transferProgress = 0;
-
     [RelayCommand]
-    private void PauseTransfer()
-    {
-        this.ChangePage(this, new ErrorViewModel("Not implemented"));
-    }
+    private void PauseTransfer() { this.ChangePage(this, new ErrorViewModel("Not implemented")); }
 
     [RelayCommand]
     private void StopTransfer()
     {
         // TODO implement stopping logic in model
         this.ChangePage(this, new StartScreenViewModel());
-    }
-
-    public ServerViewModel()
-    {
     }
 }
