50787e030646a0813f8ec1d885f6e7e14c4e8838
ed522 <147557022+ed522@users.noreply.github.com>
2026-05-28 00:14:18 -0400

    Add PasswordModal, which contains a single field for a password

---
+++ AdLib/View/Modal/PasswordModal.axaml.cs
@@ -0,0 +1,8 @@
+using Avalonia.Controls;
+
+namespace AdLib.View.Modal;
+
+public partial class PasswordModal : UserControl
+{
+    public PasswordModal() { this.InitializeComponent(); }
+}
---
+++ AdLib/ViewModel/Modal/PasswordModalViewModel.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+using AdLib.View.Modal;
+using AdLib.ViewModel.Core;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AdLib.ViewModel.Modal;
+
+public partial class PasswordModalViewModel(string title, string message) : ModalViewModel
+{
+    [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)]
+    public override Type ViewType => typeof(PasswordModal);
+
+    public override string Title { get; } = title;
+    public override bool CanCloseWithoutAction => true;
+    public string Message { get; } = message;
+
+    [ObservableProperty] public string _password = "";
+
+    public void Close() =>
+        this.Closed?.Invoke(this, new ClosedEventArgs
+        {
+            Action = CloseAction.ClosedWithoutAction,
+            Data = this,
+        });
+
+    public void CloseWithReason(CloseAction action) =>
+        this.Closed?.Invoke(this, new ClosedEventArgs
+        {
+            Action = action,
+            Data = this,
+        });
+
+    public void OnCancel() => this.CloseWithReason(CloseAction.Cancel);
+    public void OnSubmit() => this.CloseWithReason(CloseAction.Submit);
+
+    public override event EventHandler<ClosedEventArgs>? Closed;
+
+    public override event EventHandler<SwitchedEventArgs>? Switched
+    {
+        add { }
+        remove { }
+    }
+}
