3730e375878a6a08a957dabf423c29c765a84756
ed522 <147557022+ed522@users.noreply.github.com>
2026-05-28 00:09:07 -0400

    Fix build issue introduced with move

--- AdLib/View/Modal/UserPasswordModal.axaml.cs
+++ AdLib/View/Modal/UserPasswordModal.axaml.cs
@@ -2,7 +2,7 @@ using Avalonia.Controls;
 
 namespace AdLib.View.Modal;
 
-public class UserPasswordModal : UserControl
+public partial class UserPasswordModal : UserControl
 {
     public UserPasswordModal() { this.InitializeComponent(); }
 }
