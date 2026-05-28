326cfd9a875c5bfed7a2627d2b490a6dd14a2699
ed522 <147557022+ed522@users.noreply.github.com>
2026-05-27 21:27:41 -0400

    Revert "Fix strange compiler error"
    This reverts commit 7775607c9442b832a59440a0d82902f7fbb26c40.

--- AdLib/App.axaml.cs
+++ AdLib/App.axaml.cs
@@ -1,13 +1,14 @@
 using AdLib.View;
 using AdLib.ViewModel;
 
+using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
 
 namespace AdLib;
 
-public partial class App
+public class App : Application
 {
     public static readonly IBrush BackgroundBrush = new SolidColorBrush(Color.Parse("#252530"));
     public static readonly IBrush DimmedBackgroundBrush = new SolidColorBrush(Color.Parse("#101010"), 0.7);
